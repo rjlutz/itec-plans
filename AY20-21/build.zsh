721db02ec943c6db14482cf153c2b33df6ad6050
Bob Lutz <rjlutz@gmail.com>
2021-04-06 11:59:32 -0400

    add Digital Media updates for AY20-21, darken 1001 dependency arrows in ALL graphs.

--- AY20-21/build.zsh
+++ AY20-21/build.zsh
@@ -1,17 +1,7 @@
 #! /bin/zsh
-#  build.zsh
 
-# dot -Tpdf SoftwareDevelopment.dot -o outputs/SoftwareDevelopment.pdf
-# dot -Tpdf SystemsSecurity.dot -o outputs/SystemsSecurity.pdf
-# dot -Tpdf EnterpriseSystems.dot -o outputs/EnterpriseSystems.pdf
-# dot -Tpdf DataScienceAndAnalytics.dot -o outputs/DataScienceAndAnalytics.pdf
-# dot -Tpdf DigitalMedia.dot -o outputs/DigitalMedia.pdf
- 
+tracks=(SoftwareDevelopment SystemsSecurity EnterpriseSystems DataScienceAndAnalytics DigitalMedia)
 
-##tracks=(SoftwareDevelopment SystemsSecurity EnterpriseSystems DataScienceAndAnalytics DigitalMedia)    
-
-## still need to update Digital Media to latest program, courses and dependencies
-tracks=(SoftwareDevelopment SystemsSecurity EnterpriseSystems DataScienceAndAnalytics) 
 for track in $tracks; 
 do
   print "Processing ${track}.dot";
